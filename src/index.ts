e609573942897543a8fac0ef779fd577b71ca3c3
N Tamez <nathan.tamez@vonage.com>
2022-09-05 17:01:57 +0100

    fixed ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,62 @@
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
 import expressWs, { Application } from 'express-ws';
-import WebSocket from 'ws'
+import WebSocket from 'ws';
+
+interface Nexmo {
+    generateBEToken(): string;
+    generateUserToken(): string;
+    logger: any;
+    csClient: any;
+    storageClient: any;
+}
+
+type WebhookReq<T=any> = Request & {
+    nexmo: Nexmo
+}
+
+type WebhookRes = Response;
+
+type WebhookHandler = (req: WebhookReq, res: WebhookRes, next: WebhookHandler) => any | Promise<any>;
 
 const DATACENTER = `https://api.nexmo.com`
 
-const route = (app: Application, express: Express) => {
+
+export const voiceEvent: WebhookHandler = async (req, res, next) => {
+    const { logger } = req.nexmo;
+    try { 
+        logger.info("event", { event: req.body});
+        res.json({});
+    } catch (err) {
+        logger.error("Error on voiceEvent function")
+    }
+}
+
+export const voiceAnswer: WebhookHandler = async (req, res, next) => {
+    const { logger } = req.nexmo;
+    logger.info("req", { req_body: req.body});
+    try {
+        return res.json([
+            {
+                "action": "talk",
+                "text": "Please wait while we connect you to the echo server"
+            },
+            {
+                "action": "connect",
+                "from": "NexmoTest",
+                "endpoint": [{
+                    "type": "websocket",
+                    "uri": `wss://${req.hostname}/socket`,
+                    "content-type": "audio/l16;rate=16000",
+                }]
+            }
+        ]);
+    } catch (err) {
+        logger.error("Error on voiceAnswer function");
+    }
+}
+
+
+export const route = (app: Application, express: Express) => {
     const eWs = expressWs(app);
 
 
@@ -21,9 +73,3 @@ const route = (app: Application, express: Express) => {
         });
     });
 };
-
-
-
-module.exports = {
-    route
-}
